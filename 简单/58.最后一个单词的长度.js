e918978720a4fdecfca3156160e78eec13091e8f
rongwei <rongwei_alisa@hotmail.com>
2021-04-12 17:41:18 +0800

    refactor: 增加 58.最后一个单词的长度 题解

--- 简单/58.最后一个单词的长度.js
+++ 简单/58.最后一个单词的长度.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-12 17:15:57
- * @LastEditTime: 2021-04-12 17:23:38
+ * @LastEditTime: 2021-04-12 17:39:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /LeetCode/简单/58.最后一个单词的长度.js
@@ -18,8 +18,16 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-  var splitS = s.trim().split(' ');
-  return splitS[splitS.length - 1].length;
+  // var splitS = s.trim().split(' ');
+  // return splitS[splitS.length - 1].length;
+
+  // return s.trim().split(' ').pop().length;
+
+  var end = s.length - 1;
+  while (end >= 0 && s[end] === ' ') end--;
+  var start = end;
+  while (start >= 0 && s[start] !== ' ') start--;
+  return end - start;
 };
 // @lc code=end
 
