30ce1df42fe3fad6138331f3211a0a6ec44a09c7
rongwei <rongwei_alisa@hotmail.com>
2021-05-26 19:19:42 +0800

    feat: 136.只出现一次的数字-位运算

--- 简单/136.只出现一次的数字.js
+++ 简单/136.只出现一次的数字.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-05-26 09:21:22
- * @LastEditTime: 2021-05-26 19:13:46
+ * @LastEditTime: 2021-05-26 19:14:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /LeetCode/简单/136.只出现一次的数字.js
@@ -18,22 +18,22 @@
  * @return {number}
  */
 // 方法一
-// var singleNumber = function (nums) {
-//   var obj = {};
-//   for (var i = 0; i < nums.length; i++) {
-//     if (!obj[nums[i]]) {
-//       obj[nums[i]] = 1;
-//     } else {
-//       obj[nums[i]]++;
-//     }
-//   }
-//   for (var i in obj) {
-//     if (obj[i] === 1) {
-//       return i;
-//     }
-//     continue;
-//   }
-// };
+var singleNumber = function (nums) {
+  var obj = {};
+  for (var i = 0; i < nums.length; i++) {
+    if (!obj[nums[i]]) {
+      obj[nums[i]] = 1;
+    } else {
+      obj[nums[i]]++;
+    }
+  }
+  for (var i in obj) {
+    if (obj[i] === 1) {
+      return i;
+    }
+    continue;
+  }
+};
 
 // 方法二-使用位运算符
 var singleNumber = function (nums) {
