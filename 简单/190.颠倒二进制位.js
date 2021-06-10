e5fefbc514b254f04cf8a30ac237ef6980e386a9
rongwei <rongwei_alisa@hotmail.com>
2021-06-10 23:05:23 +0800

    feat: 颠倒二进制位

--- 简单/190.颠倒二进制位.js
+++ 简单/190.颠倒二进制位.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-10 09:33:00
- * @LastEditTime: 2021-06-10 23:03:28
+ * @LastEditTime: 2021-06-10 23:05:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /LeetCode/简单/190.颠倒二进制位.js
@@ -18,7 +18,13 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function (n) {
-  console.log('n', n);
+  var binaryStr = n.toString(2);
+  binaryStr = '0'.repeat(32 - binaryStr.length) + binaryStr;
+
+  // 二进制转十进制
+  var resultBinary = binaryStr.split('').reverse().join('');
+  return parseInt(resultBinary, 2);
+
   // 十进制转颠倒二进制字符串
   // var binaryStr = '', num = n, remainder = 0;
   // while (num) {
@@ -26,13 +32,7 @@ var reverseBits = function (n) {
   //   num = num >> 1;
   //   binaryStr = remainder + binaryStr;
   // }
-  var binaryStr = n.toString(2);
-  binaryStr = '0'.repeat(32 - binaryStr.length) + binaryStr;
-  console.log('binaryStr', binaryStr);
 
-  // 二进制转十进制
-  var resultBinary = binaryStr.split('').reverse().join('');
-  return parseInt(resultBinary, 2);
   // for (var i = 0; i < 32; i++) {
   //   if (binaryStr[i] === '0') continue;
   //   result += Math.pow(2, i);
