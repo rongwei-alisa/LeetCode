ccead4a86f9729ccc69b56b5212cd21ddd232393
rongwei <rongwei_alisa@hotmail.com>
2021-07-13 20:44:20 +0800

    feat: 257。二叉树的所有路径-广度优先

--- 简单/257.二叉树的所有路径.js
+++ 简单/257.二叉树的所有路径.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-07 20:41:27
- * @LastEditTime: 2021-07-13 20:28:44
+ * @LastEditTime: 2021-07-13 20:43:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /LeetCode/简单/257.二叉树的所有路径.js
@@ -25,11 +25,12 @@
  * @param {TreeNode} root
  * @return {string[]}
  */
+// 深度优先
 var binaryTreePaths = function (root) {
   var paths = [];
   var constructPaths = function (node, path) {
     if (node) {
-      path += node.val + '';
+      path += node.val;
       if (node.left === null && node.right === null) {
         paths.push(path);
       } else {
@@ -42,5 +43,28 @@ var binaryTreePaths = function (root) {
   constructPaths(root, '');
   return paths;
 };
+
+// 广度优先
+var binaryTreePaths = function (root) {
+  if (root === null) return [];
+  var paths = [], nodeArr = [root], pathArr = [`${root.val}`];
+  while (nodeArr.length) {
+    var node = nodeArr.shift(), path = pathArr.shift();
+    if (node.left === null && node.right === null) {
+      paths.push(path);
+    } else {
+      path += '->';
+      if (node.left) {
+        nodeArr.push(node.left);
+        pathArr.push(path + node.left.val);
+      }
+      if (node.right) {
+        nodeArr.push(node.right);
+        pathArr.push(path + node.right.val);
+      }
+    }
+  }
+  return paths;
+}
 // @lc code=end
 
