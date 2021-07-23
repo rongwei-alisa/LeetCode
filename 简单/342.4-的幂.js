11497ad49eb8ec05fc20815e5cf7103a7d0d9bbd
rongwei <rongwei_alisa@hotmail.com>
2021-07-23 09:46:00 +0800

    feat: 342.4-的幂

--- 简单/342.4-的幂.js
+++ 简单/342.4-的幂.js
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2021-07-22 09:32:29
+ * @LastEditTime: 2021-07-23 09:45:27
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /LeetCode/简单/342.4-的幂.js
+ */
+/*
  * @lc app=leetcode.cn id=342 lang=javascript
  *
  * [342] 4的幂
@@ -9,8 +17,13 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
-
+var isPowerOfFour = function (n) {
+  while (n >= 1) {
+    if (n === 1) return true;
+    if (n % 4 !== 0) return false;
+    n = n / 4;
+  }
+  return false;
 };
 // @lc code=end
 
