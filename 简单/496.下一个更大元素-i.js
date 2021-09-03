f489c60ca4d25d46455a3bdc041962a70dfaf62c
rongwei <rongwei_alisa@hotmail.com>
2021-09-03 10:52:37 +0800

    feat: 496. 下一个更大元素 - 栈

--- 简单/496.下一个更大元素-i.js
+++ 简单/496.下一个更大元素-i.js
@@ -2,7 +2,7 @@
  * @Author: rongwei
  * @Date: 2021-09-03 09:18:52
  * @LastEditors: rongwei
- * @LastEditTime: 2021-09-03 10:28:12
+ * @LastEditTime: 2021-09-03 10:49:54
  * @Description: file content
  */
 /*
@@ -17,26 +17,44 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+// var nextGreaterElement = function (nums1, nums2) {
+//   var map = {};
+//   for (var i = 0; i < nums2.length; i++) {
+//     if (i === nums2.length - 1) {
+//       map[nums2[i]] = -1;
+//       break;
+//     }
+//     for (var j = i + 1; j < nums2.length; j++) {
+//       if (nums2[j] > nums2[i]) {
+//         map[nums2[i]] = nums2[j];
+//         break;
+//       } else {
+//         map[nums2[i]] = -1;
+//       }
+//     }
+//   }
+
+//   var result = [];
+//   for (var m = 0; m < nums1.length; m++) {
+//     result.push(map[nums1[m]]);
+//   }
+
+//   return result;
+// };
+
 var nextGreaterElement = function (nums1, nums2) {
-  var map = {};
+  var numsMap = {}, stack = [];
   for (var i = 0; i < nums2.length; i++) {
-    if (i === nums2.length - 1) {
-      map[nums2[i]] = -1;
-      break;
+    while (stack.length !== 0 && nums2[i] > stack[stack.length - 1]) {
+      var popNum = stack.pop();
+      numsMap[popNum] = nums2[i];
     }
-    for (var j = i + 1; j < nums2.length; j++) {
-      if (nums2[j] > nums2[i]) {
-        map[nums2[i]] = nums2[j];
-        break;
-      } else {
-        map[nums2[i]] = -1;
-      }
-    }
+    stack.push(nums2[i]);
   }
 
   var result = [];
   for (var m = 0; m < nums1.length; m++) {
-    result.push(map[nums1[m]]);
+    result.push(numsMap[nums1[m]] ?? -1);
   }
 
   return result;
