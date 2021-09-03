43192ac80a47a9b3d72003e18c855307035da0b3
rongwei <rongwei_alisa@hotmail.com>
2021-09-03 10:28:55 +0800

    feat: 496.下一个更大元素

---
+++ 简单/496.下一个更大元素-i.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: rongwei
+ * @Date: 2021-09-03 09:18:52
+ * @LastEditors: rongwei
+ * @LastEditTime: 2021-09-03 10:28:12
+ * @Description: file content
+ */
+/*
+ * @lc app=leetcode.cn id=496 lang=javascript
+ *
+ * [496] 下一个更大元素 I
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function (nums1, nums2) {
+  var map = {};
+  for (var i = 0; i < nums2.length; i++) {
+    if (i === nums2.length - 1) {
+      map[nums2[i]] = -1;
+      break;
+    }
+    for (var j = i + 1; j < nums2.length; j++) {
+      if (nums2[j] > nums2[i]) {
+        map[nums2[i]] = nums2[j];
+        break;
+      } else {
+        map[nums2[i]] = -1;
+      }
+    }
+  }
+
+  var result = [];
+  for (var m = 0; m < nums1.length; m++) {
+    result.push(map[nums1[m]]);
+  }
+
+  return result;
+};
+// @lc code=end
+
