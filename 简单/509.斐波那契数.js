469d3c94a08a4c524b91ca8fef91a7faf3966e7a
rongwei <rongwei_alisa@hotmail.com>
2021-09-14 10:18:49 +0800

    feat: 509.520

--- 简单/509.斐波那契数.js
+++ 简单/509.斐波那契数.js
@@ -2,7 +2,7 @@
  * @Author: RongWei
  * @Date: 2021-09-13 09:32:46
  * @LastEditors: RongWei
- * @LastEditTime: 2021-09-13 09:35:52
+ * @LastEditTime: 2021-09-14 10:16:48
  * @Description: file content
  */
 /*
@@ -16,10 +16,24 @@
  * @param {number} n
  * @return {number}
  */
+// 递归
+// var fib = function (n) {
+//   if (n === 0) return 0;
+//   if (n === 1) return 1;
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// 递推
 var fib = function (n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  return fib(n - 1) + fib(n - 2);
-};
+  var arr = new Array();
+  for (var i = 0; i <= n; i++) {
+    if (i < 2) {
+      arr[i] = i;
+    } else {
+      arr[i] = arr[i - 1] + arr[i - 2];
+    }
+  }
+  return arr[n];
+}
 // @lc code=end
 
--- 简单/520.检测大写字母.js
+++ 简单/520.检测大写字母.js
@@ -2,7 +2,7 @@
  * @Author: RongWei
  * @Date: 2021-09-14 09:17:58
  * @LastEditors: RongWei
- * @LastEditTime: 2021-09-14 09:34:40
+ * @LastEditTime: 2021-09-14 09:50:13
  * @Description: file content
  */
 /*
@@ -16,24 +16,35 @@
  * @param {string} word
  * @return {boolean}
  */
+// var detectCapitalUse = function (word) {
+//   if (word.length === 1) return true;
+
+//   var firstLetter = word.charCodeAt(0) >= 97 ? 'S' : 'B';
+//   var secondLetter = word.charCodeAt(1) >= 97 ? 'S' : 'B';
+//   if (firstLetter === 'S' && secondLetter === 'B') return false;
+
+//   for (var i = 2; i < word.length; i++) {
+//     if (firstLetter === 'B' && secondLetter === 'B') {
+//       if (word.charCodeAt(i) >= 97) {
+//         return false;
+//       }
+//     } else if (word.charCodeAt(i) <= 90) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
+
+// 正则
 var detectCapitalUse = function (word) {
-  if (word.length === 1) return true;
-
-  var firstLetter = word.charCodeAt(0) >= 97 ? 'S' : 'B';
-  var secondLetter = word.charCodeAt(1) >= 97 ? 'S' : 'B';
-  if (firstLetter === 'S' && secondLetter === 'B') return false;
-
-  for (var i = 2; i < word.length; i++) {
-    if (firstLetter === 'B' && secondLetter === 'B') {
-      if (word.charCodeAt(i) >= 97) {
-        return false;
-      }
-    } else if (word.charCodeAt(i) <= 90) {
-      return false;
-    }
-  }
-
-  return true;
+  // 全大写
+  var reg1 = /^[A-Z]*$/;
+  // 全小写
+  var reg2 = /^[a-z]*$/;
+  // 首字母大写
+  var reg3 = /^[A-Z][a-z]+$/;
+  return reg1.test(word) || reg2.test(word) || reg3.test(word);
 };
 // @lc code=end
 
