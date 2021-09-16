f8b0cd963d5c617dfbcdeaddc8b18ec5fe374c92
rongwei <rongwei_alisa@hotmail.com>
2021-09-16 10:01:11 +0800

    feat: 530.二叉搜索树的最小绝对差

--- 简单/530.二叉搜索树的最小绝对差.js
+++ 简单/530.二叉搜索树的最小绝对差.js
@@ -2,7 +2,7 @@
  * @Author: RongWei
  * @Date: 2021-09-16 09:20:35
  * @LastEditors: RongWei
- * @LastEditTime: 2021-09-16 09:33:12
+ * @LastEditTime: 2021-09-16 10:00:18
  * @Description: file content
  */
 /*
@@ -24,24 +24,43 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// var getMinimumDifference = function (root) {
+//   var sortedVal = [];
+//   var iterateeTree = function (node) {
+//     if (node === null) return;
+//     iterateeTree(node.left);
+//     sortedVal.push(node.val);
+//     iterateeTree(node.right);
+//   };
+
+//   iterateeTree(root);
+
+//   var result = Math.abs(sortedVal[1] - sortedVal[0]);
+//   for (var i = 2; i < sortedVal.length; i++) {
+//     var diff = Math.abs(sortedVal[i] - sortedVal[i - 1]);
+//     if (diff < result) {
+//       result = diff;
+//     }
+//   }
+//   return result;
+// };
+
 var getMinimumDifference = function (root) {
-  var sortedVal = [];
+  var result = Number.MAX_SAFE_INTEGER, pre;
   var iterateeTree = function (node) {
     if (node === null) return;
     iterateeTree(node.left);
-    sortedVal.push(node.val);
+    if (!pre && pre !== 0) {
+      pre = node.val;
+    } else {
+      result = Math.min(node.val - pre, result);
+      pre = node.val;
+    }
     iterateeTree(node.right);
   };
 
   iterateeTree(root);
 
-  var result = Math.abs(sortedVal[1] - sortedVal[0]);
-  for (var i = 2; i < sortedVal.length; i++) {
-    var diff = Math.abs(sortedVal[i] - sortedVal[i - 1]);
-    if (diff < result) {
-      result = diff;
-    }
-  }
   return result;
 };
 // @lc code=end
