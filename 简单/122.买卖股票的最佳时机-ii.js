0c4c392735ec58ad017287ef7a0fce04518aca34
rongwei <rongwei_alisa@hotmail.com>
2024-04-17 09:33:48 +0800

    Update 122.买卖股票的最佳时机-ii.js

--- 简单/122.买卖股票的最佳时机-ii.js
+++ 简单/122.买卖股票的最佳时机-ii.js
@@ -1,10 +1,5 @@
 /*
- * @Author: your name
- * @Date: 2021-05-24 09:34:49
- * @LastEditTime: 2021-05-24 10:00:23
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /LeetCode/简单/122.买卖股票的最佳时机-ii.js
+ * 贪心
  */
 /*
  * @lc app=leetcode.cn id=122 lang=javascript
@@ -17,17 +12,31 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
-  var curMin = prices[0], profit = 0;
-  for (var i = 1; i < prices.length; i++) {
-    if (prices[i] < prices[i - 1]) {
-      profit += prices[i - 1] - curMin;
-      curMin = prices[i];
-    } else if (i === prices.length - 1) {
-      profit += prices[i] - curMin;
-    }
+// var maxProfit = function (prices) {
+//   var curMin = prices[0], profit = 0;
+//   for (var i = 1; i < prices.length; i++) {
+//     if (prices[i] < prices[i - 1]) {
+//       profit += prices[i - 1] - curMin;
+//       curMin = prices[i];
+//     } else if (i === prices.length - 1) {
+//       profit += prices[i] - curMin;
+//     }
+//   }
+//   return profit;
+// };
+
+var maxProfit = function(prices) {
+  let minPrice = prices[0];
+  let res = 0;
+  for (let i = 1; i < prices.length; i++) {
+      if (prices[i] > minPrice) {
+          res += prices[i] - minPrice;
+          minPrice = prices[i];
+      } else {
+          minPrice = prices[i];
+      }
   }
-  return profit;
+  return res;
 };
 // @lc code=end
 
